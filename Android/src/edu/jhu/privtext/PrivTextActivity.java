811615fd33a6908a0664c13b850cdf8ab8d546d7
Gary Belvin <gbelvin1@jhu.edu>
2011-06-17 15:03:52 -0400

    Android

---
+++ Android/src/edu/jhu/privtext/PrivTextActivity.java
@@ -0,0 +1,13 @@
+package edu.jhu.privtext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PrivTextActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
