6d7a7b6b55dbf6ea2c660e49ae9341ffec7f576c
Gary Belvin <gbelvin1@jhu.edu>
2011-08-03 15:22:00 -0400

    Added Skein

---
+++ Android/src/edu/jhu/bouncycastle/util/encoders/Hex.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2000 - 2011 The Legion Of The Bouncy Castle
+ * (http://www.bouncycastle.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package edu.jhu.bouncycastle.util.encoders;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Hex {
+	private static final Encoder encoder = new HexEncoder();
+
+	/**
+	 * encode the input data producing a Hex encoded byte array.
+	 *
+	 * @return a byte array containing the Hex encoded data.
+	 */
+	public static byte[] encode(byte[] data) {
+		return encode(data, 0, data.length);
+	}
+
+	/**
+	 * encode the input data producing a Hex encoded byte array.
+	 *
+	 * @return a byte array containing the Hex encoded data.
+	 */
+	public static byte[] encode(byte[] data, int off, int length) {
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+		try {
+			encoder.encode(data, off, length, bOut);
+		} catch (IOException e) {
+			throw new RuntimeException("exception encoding Hex string: " + e);
+		}
+
+		return bOut.toByteArray();
+	}
+
+	/**
+	 * Hex encode the byte data writing it to the given output stream.
+	 *
+	 * @return the number of bytes produced.
+	 */
+	public static int encode(byte[] data, OutputStream out) throws IOException {
+		return encoder.encode(data, 0, data.length, out);
+	}
+
+	/**
+	 * Hex encode the byte data writing it to the given output stream.
+	 *
+	 * @return the number of bytes produced.
+	 */
+	public static int encode(byte[] data, int off, int length, OutputStream out)
+			throws IOException {
+		return encoder.encode(data, off, length, out);
+	}
+
+	/**
+	 * decode the Hex encoded input data. It is assumed the input data is valid.
+	 *
+	 * @return a byte array representing the decoded data.
+	 */
+	public static byte[] decode(byte[] data) {
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+		try {
+			encoder.decode(data, 0, data.length, bOut);
+		} catch (IOException e) {
+			throw new RuntimeException("exception decoding Hex string: " + e);
+		}
+
+		return bOut.toByteArray();
+	}
+
+	/**
+	 * decode the Hex encoded String data - whitespace will be ignored.
+	 *
+	 * @return a byte array representing the decoded data.
+	 */
+	public static byte[] decode(String data) {
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+		try {
+			encoder.decode(data, bOut);
+		} catch (IOException e) {
+			throw new RuntimeException("exception decoding Hex string: " + e);
+		}
+
+		return bOut.toByteArray();
+	}
+
+	/**
+	 * decode the Hex encoded String data writing it to the given output stream,
+	 * whitespace characters will be ignored.
+	 *
+	 * @return the number of bytes produced.
+	 */
+	public static int decode(String data, OutputStream out) throws IOException {
+		return encoder.decode(data, out);
+	}
+
+	public static String printHex(byte[] theData) {
+		byte[] hex = Hex.encode(theData);
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < hex.length; i++) {
+			char c = (char) hex[i];
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+}
